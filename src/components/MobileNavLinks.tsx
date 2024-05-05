9db19aed43c1a36fc98d4abc9176e930d930bc83
Nanamoriyama <nanamoriyama@outlook.jp>
2024-05-05 23:20:34 +0200

    second commit

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -3,16 +3,24 @@ import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const MobileNavLinks = () => {
-  const { logout } = useAuth0;
+  const { logout } = useAuth0();
   return (
     <>
       <Link
+        to="/order-status"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Order Status
+      </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        My Restaurant
+      </Link>
+      <Link
         to="/user-profile"
-        className="
-        bg-white
-        items-center
-        font-bold
-        hover:text-orange-500"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
       >
         User Profile
       </Link>
